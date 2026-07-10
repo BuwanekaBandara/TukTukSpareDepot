e9e6f7ea5e46c296b1b3dad5d708044b43d90c5f
BuwanekaBandara <charu.20250117@iit.ac.lk>
2026-07-10 21:44:09 +0530

    Creation of methods to update the parts objects and find low stock items.

--- src/main/java/org/example/tuktuksparedepot/InventoryOp.java
+++ src/main/java/org/example/tuktuksparedepot/InventoryOp.java
@@ -35,6 +35,64 @@ public class InventoryOp {
         System.out.println("The part code does not exist");
     }
 
+    public void updatePart(String partCode, String newPartName, String newPartBrand, double newPartPrice, int newPartQuantity, String newPartCategory,String newPartDate, String newPartImg){
+        sparePart part=getPartByCode(partCode);
+        if (part==null){
+            System.out.println("The part code does not exist");
+            return;
+        }
+
+        if(newPartName!=null) {
+            part.setPartName(newPartName);
+        }
+        else{
+            part.setPartName(part.getPartName());
+        }
+
+        if (newPartBrand != null){
+            part.setBrand(newPartBrand);
+        }
+        else{
+            part.setBrand(part.getBrand());
+        }
+
+        if (newPartPrice >=0){
+            part.setPrice(newPartPrice);
+        }
+        else{
+            part.setPrice(part.getPrice());
+        }
+
+        if (newPartQuantity >=0){
+            part.setQuantity(newPartQuantity);
+        }
+        else{
+            part.setQuantity(part.getQuantity());
+        }
+
+        if(newPartCategory!=null){
+            part.setCategory(newPartCategory);
+        }
+        else{
+            part.setCategory(part.getCategory());
+        }
+
+        if (newPartDate != null){
+            part.setDate(newPartDate);
+        }
+        else{
+            part.setDate(part.getDate());
+        }
+
+        if (newPartImg!=null){
+            part.setImg(newPartImg);
+        }
+        else{
+            part.setImg(part.getImg());
+        }
+        System.out.println("Part updated successfully");
+    }
+
     public sparePart getPartByCode(String matchPart){
         for(int i=0;i<parts.size();i++){
             if(parts.get(i).getPartCode().equals(matchPart.trim().toUpperCase())){
@@ -44,5 +102,17 @@ public class InventoryOp {
         System.out.println("The part code does not exist");
         return null;
     }
+
+    public ArrayList<sparePart> getLowStockParts(){
+        ArrayList<sparePart> lowStock= new ArrayList<sparePart>();
+        for(int i=0;i<parts.size();i++){
+            if(parts.get(i).getQuantity()<=3){
+                lowStock.add(parts.get(i));
+            }
+        }
+        return lowStock;
+    }
+
+
 
 }
