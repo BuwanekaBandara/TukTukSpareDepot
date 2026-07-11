0b60ee018a65cea3cfd71cde78e1ff93480e545d
BuwanekaBandara <charu.20250117@iit.ac.lk>
2026-07-11 22:16:24 +0530

    Creation of cartItem java class.

---
+++ src/main/java/org/example/tuktuksparedepot/objects/cartItem.java
@@ -0,0 +1,48 @@
+package org.example.tuktuksparedepot.objects;
+
+public class cartItem {
+
+    private sparePart part;
+    private int quantity;
+
+    public cartItem(sparePart part, int quantity) {
+        setPart(part);
+        if (part!=null) {
+            setCartQuantity(quantity);
+        }
+    }
+
+    public sparePart getPart(){
+        return part;
+    }
+    public void setPart(sparePart part){
+        if(part == null){
+            System.out.println("The part cannot be empty");
+            return;
+        }
+        this.part=part;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+    public void setCartQuantity(int quantity){
+        if(quantity<=0 || quantity> part.getQuantity()){
+            System.out.println("Invalid quantity");
+            return;
+        }
+        else{
+            this.quantity=quantity;
+        }
+    }
+
+    public double getSubTotal(){
+        if (part==null || quantity<=0){
+            return 0;
+        }
+        double subTotal=part.getPrice()*quantity;
+        return subTotal;
+    }
+
+
+}
